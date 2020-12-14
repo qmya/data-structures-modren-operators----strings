13d870fd4c9deee98128b6e5504e3cce849d49dc
qmya <qandeelgul90@gmail.com>
2020-12-14 09:49:19 -0500

    destructuring Arrays

--- script.js
+++ script.js
@@ -6,19 +6,38 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
 };
+//Without Destructuring
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+//With destructuring
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+//Destructing in the above object and its array
+const [first,second] = restaurant.categories;
+console.log(first, second);
+
+//if you wat to skip the second element 
+// const [first, ,second] = restaurant.categories;
+// console.log(first, second);
+
+
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
+// };
